eb7b0333c330bdb7e2b946f6818e43e19f15b927
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2023-11-11 19:14:42 -0500

    setting up handling commands

--- my_calendar.py
+++ my_calendar.py
@@ -2,6 +2,61 @@
 # This is a project that will keep track of assignments and show progress towards when they are due
 # Author: Kellen Bell
 
-def main():
+import sys
+import csv
+
+# gets all of the events
+def getEvents():
+    file = open('calendar.csv')
+    type(file)
+
+    reader = csv.reader(file)
+
+    headers = next(reader)
+
+    print(headers)
+
+    file.close()
+
+# add an event to the list of events
+def addEvent():
     pass
 
+# we will take in different commands based on functionality we want
+def handleArgs():
+
+    # get rid of file name arg
+    args = sys.argv[1:]
+
+    # make sure we have a valid number of args
+    n = len(args)
+
+    if n != 1:
+        print("Error: Invalid number of arguments\n")
+        printHelpMessage()
+        return
+
+    command = args[0]
+
+    if command == "h":
+        printHelpMessage()
+        return
+
+    if command == "a":
+        addEvent()
+
+    if command == "g":
+        getEvents()
+
+# print the help message
+def printHelpMessage():
+    print("Expected format of command:")
+    print("\tpython my_calendar -function")
+    print("\n\tFunction may be:")
+    print("\t\ta: add an event\n\t\tg: get event\n\t\th: help")
+
+def main():
+    handleArgs()
+
+if __name__ == "__main__":
+    main()
